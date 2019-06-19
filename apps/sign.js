dc8890b56a007de1791f578a0969e379afc24c88
Mauro Leggieri <mxmauro@mauroleggieri.com>
2019-06-19 18:04:50 -0300

    Better signer app options.

--- apps/sign.js
+++ apps/sign.js
@@ -1,5 +1,6 @@
 const cmdline = require('node-cmdline-parser');
 const tools = require('../index');
+const rl = require('readline-sync');
 
 //------------------------------------------------------------------------------
 
@@ -18,11 +19,11 @@ async function main() {
 		console.log("Use: sign.js parameters [options]");
 		console.log("");
 		console.log("Where 'parameters' are:");
-		console.log("  --input {FILENAME}      : File with transactions to sign.");
-		console.log("  --output {FILENAME}     : Output file to create with signed transactions.");
-		console.log("  --mnemonic \"{MNEMONIC}\" : Signer's mnemonic. Enclose the 25-word passphrase in quotes.");
+		console.log("  --input {FILENAME} or {FOLDERNAME} : Folder and/or file with transactions to sign. Wildcards accepted on filename.");
+		console.log("  --output {FILENAME}                : Output file to create with signed transactions. Can be used only if input is only one file.");
 		console.log("");
 		console.log("And 'options' are:");
+		console.log("  --mnemonic \"{MNEMONIC}\"                     : Signer's mnemonic. Enclose the 25-word passphrase in quotes. If not provided, the app will ask for it.");
 		console.log("  --multisig-threshold {NUMBER}               : Required signatures for a multsig account.");
 		console.log("  --multisig-addresses {ADDRESS[,ADDRESS...]} : A comma separated list of addresses that make up the multisig account. Required only for the first signature.");
 		console.log("  --remove-existing                           : Remove any previously existing signature from the transaction.");
@@ -31,59 +32,104 @@ async function main() {
 
 	let options = await parseCmdLineParams();
 
-	let txs = await tools.storage.loadTransactionsFromFile(options.input);
-
-	for (let idx = 0; idx < txs.length; idx++) {
-		if (options.remove_signature) {
-			tools.sign.removeAllSignatures(txs[idx]);
-		}
-		tools.sign.addSignature(txs[idx], options.mnemonic, options.multisig_threshold, options.multisig_addresses);
+	if (!(options.remove_signature || options.mnemonic)) {
+		//ask for mnemonic
+		options.mnemonic = rl.question('Enter mnemonic: ');
 	}
 
-	await tools.storage.saveTransactionsToFile(options.output, txs);
+	for (let idx = 0; idx < options.input.length; idx++) {
+		console.log('Loading ' + options.input[idx] + '...');
+		let txs = await tools.storage.loadTransactionsFromFile(options.input[idx]);
+
+		for (let tx_idx = 0; tx_idx < txs.length; tx_idx++) {
+			let msg = 'Processing ' + (tx_idx + 1).toString() + ' of ' + txs.length.toString() + '...';
+			console.log(msg);
+
+			if (options.remove_signature) {
+				tools.sign.removeAllSignatures(txs[tx_idx]);
+			}
+			tools.sign.addSignature(txs[idx], options.mnemonic, options.multisig_threshold, options.multisig_addresses);
+		}
+
+		console.log('Saving ' + options.output[idx] + '...');
+		await tools.storage.saveTransactionsToFile(options.output[idx], txs);
+	}
 }
 
 function parseCmdLineParams() {
 	return new Promise((resolve, reject) => {
-		let input = cmdline.get('input');
-		if (input === null) {
+		let idx, idx2;
+
+		let filemask = cmdline.get('input');
+		if (filemask === null) {
 			reject(new Error("ERROR: Missing value in '--input' parameter."));
 			return;
 		}
+		idx = filemask.lastIndexOf('/');
+		idx2 = filemask.lastIndexOf('\\');
+		if (idx2 > idx) {
+			idx = idx2;
+		}
+		let _input = {
+			folder: filemask.substr(0, idx + 1),
+			filemask: filemask.substr(idx + 1)
+		};
+		if (_input.folder.indexOf('*') >= 0 || _input.folder.indexOf('?') >= 0) {
+			reject(new Error("ERROR: Wildcards are not allowed in the folder part of the '--input' parameter."));
+			return;
+		}
 		try {
-			input = tools.utils.normalizeFilename(input);
+			_input.folder = tools.utils.normalizeFolder(_input.folder);
 		}
 		catch (err) {
 			reject(err);
 			return;
 		}
-		if (input.length == 0) {
+		if (_input.folder.length == 0) {
 			reject(new Error("ERROR: Invalid value in '--input' parameter."));
 			return;
 		}
+		if (_input.filemask.length == 0) {
+			_input.filemask = '*.tx';
+		}
+		let _inputHasWildcards = (_input.filemask.indexOf('*') >= 0 || _input.filemask.indexOf('?') >= 0);
+		let input = tools.utils.getFileList(_input.folder, _input.filemask);
 
 		let output = cmdline.get('output');
-		if (output === null) {
-			reject(new Error("ERROR: Missing value in '--output' parameter."));
-			return;
+		if (_inputHasWildcards || output === null) {
+			if (output !== null) {
+				reject(new Error("ERROR: Cannot use '--output' parameter if input is a folder or has wildcards."));
+				return;
+			}
+			output = [];
+			for (let filename of input) {
+				output.push(filename + '.sig');
+			}
 		}
-		try {
-			output = tools.utils.normalizeFilename(output);
-		}
-		catch (err) {
-			reject(err);
-			return;
-		}
-		if (output.length == 0) {
-			reject(new Error("ERROR: Invalid value in '--output' parameter."));
-			return;
+		else {
+			try {
+				output = tools.utils.normalizeFilename(output);
+			}
+			catch (err) {
+				reject(err);
+				return;
+			}
+			if (output.length == 0) {
+				reject(new Error("ERROR: Invalid value in '--output' parameter."));
+				return;
+			}
+			if (input.length > 0) {
+				if (input.length > 1) {
+					input.splice(1, input.length - 1); //NOTE: This should not happen
+				}
+				output = [ output ];
+			}
+			else {
+				output = [];
+			}
 		}
 
 		let mnemonic = cmdline.get('mnemonic');
-		if (mnemonic === null) {
-			reject(new Error("ERROR: Missing value in '--mnemonic' parameter."));
-			return;
-		}
 
 		let multisig_threshold;
 		if (cmdline.keyexists('multisig-threshold')) {
@@ -108,7 +154,7 @@ function parseCmdLineParams() {
 					reject(new Error("ERROR: Invalid value in '--multisig-addresses' parameter. It must be a comma-separated list of addresses."));
 					return;
 				}
-				for (let idx = 0; idx < multisig_addresses.length; idx++) {
+				for (idx = 0; idx < multisig_addresses.length; idx++) {
 					multisig_addresses[idx] = multisig_addresses[idx].trim();
 					if (multisig_addresses[idx].length == 0) {
 						reject(new Error("ERROR: Invalid value in '--multisig-addresses' parameter. Addresses can not be empty."));
