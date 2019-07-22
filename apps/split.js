349b7857c864b09885f65a1543cd233483d5dcf5
Mauro Leggieri <mxmauro@users.noreply.github.com>
2019-07-22 12:20:54 -0300

    Fixed bug while appending a multi signature (#17)
    * Initial release
    
    * Initial commit
    
    * Fixed first sign on multisig (fee multiplication still pending)
    
    * Using local fork of algosdk
    
    * Fixed merger and tx.getInfo
    
    * Changed first signature to use internal functions to avoid fee modifications
    
    * msgpack decoder does not support partial reads.
    
    * Added signature template method.
    
    * Fixed multisig address retrieval
    
    * Renamed apps and added raw sender
    
    * Added more options to genesis hash and notes and fee override
    
    * Fixed bug in multisig template
    
    * Fixed tx merging
    
    * Added packager and help
    
    * Build TX with multisignature template
    
    * Help in app files
    
    * Finished apps help.
    
    * Light documentation
    
    * A minor fix for sending tx (#3)
    
    * Wrong calculation if a TX should be sent (#4)
    
    * Improved output for transaction dispatch (#5)
    
    * Improved node response, tx dump and added fixed-fee option to builder. (#6)
    
    * Update README.md (#8)
    
    * Update README.md
    
    * Update README.md
    
    * Finished help
    
    * Improved sign app (#10)
    
    * Better signer app options.
    
    * Fixed add signature and threshold autocheck.
    
    * Receiver is now optional if a close address is specified.
    
    * Added Ledger device support. (#12)
    
    * Documentation of functions (#11)
    
    * Filter app (#13)
    
    * Added transaction filtering app
    
    * Incremented revision number
    
    * Improved split app (#15)
    
    * Fixed bug when multi signing a tx (#16)

--- apps/split.js
+++ apps/split.js
@@ -21,11 +21,11 @@ async function main() {
 		console.log("Use: split.js parameters");
 		console.log("");
 		console.log("Where 'parameters' are:");
-		console.log("  --input {FILENAME}  : File with transactions to split.");
-		console.log("  --from {NUMBER}     : Include transactions starting from the specified round. If first round is less than 'from' but last round is greater or equal, it is also included.");
-		console.log("  --to {NUMBER}       : Include transactions until the specified round. If first round is less or equal to 'to' but last round is greater , it is also included.");
-		console.log("  --count {NUMBER}    : Splits in files of 'count' transactions each.");
-		console.log("  --quantity {NUMBER} : Splits all transactions in the specified 'quantity' of files.");
+		console.log("  --input {FILENAME}     : File with transactions to split.");
+		console.log("  --by-count {NUMBER}    : Splits in files of 'count' transactions each.");
+		console.log("  --by-quantity {NUMBER} : Splits all transactions in the specified 'quantity' of files.");
+		console.log("  --by-sender            : Splits all transactions by sender address.");
+		console.log("  --by-receiver          : Splits all transactions by receiver address.");
 		return;
 	}
 
@@ -38,10 +38,10 @@ async function main() {
 		throw new Error('Nothing to process');
 	}
 
-	if (options.count) {
-		let files_count = Math.ceil(txs.length / options.count);
+	if (options.by_count) {
+		let files_count = Math.ceil(txs.length / options.by_count);
 		for (let idx = 0; idx < files_count; idx++) {
-			let split_txs = txs.slice(idx * options.count, (idx + 1) * options.count);
+			let split_txs = txs.slice(idx * options.by_count, (idx + 1) * options.by_count);
 
 			let output_filename = path.format({
 				dir: path_comps.dir,
@@ -52,13 +52,13 @@ async function main() {
 			await tools.storage.saveTransactionsToFile(output_filename, split_txs);
 		}
 	}
-	else if (options.quantity) {
+	else if (options.by_quantity) {
 		let start_idx = 0;
 		let end_idx;
 
-		for (let idx = 0; idx < options.quantity; idx++) {
-			if (idx < options.quantity - 1) {
-				end_idx = Math.floor(idx * (txs.length / options.quantity));
+		for (let idx = 0; idx < options.by_quantity; idx++) {
+			if (idx < options.by_quantity - 1) {
+				end_idx = Math.floor(idx * (txs.length / options.by_quantity));
 				if (end_idx < start_idx) {
 					end_idx = start_idx + 1;
 				}
@@ -86,26 +86,37 @@ async function main() {
 			start_idx = end_idx + 1;
 		}
 	}
-	else {
-		let indexes = [];
+	else if (options.by_sender || options.by_receiver) {
+		let split_txs = new Map();
+
 		for (let tx_idx = 0; tx_idx < txs.length; tx_idx++) {
 			let tx_info = tools.tx.getInfo(txs[tx_idx]);
-			if (tx_info.first_round < options.to && tx_info.last_round > options.from) {
-				indexes.push(tx_idx);
+
+			let address = options.by_sender ? tx_info.from : tx_info.to;
+
+			let indexes = split_txs.get(address);
+			if (!indexes) {
+				indexes = [];
+				split_txs.set(address, indexes);
 			}
+			indexes.push(tx_idx);
 		}
 
-		let split_txs = indexes.map((tx_idx) => {
-			return txs[tx_idx];
-		});
+		for (let address of split_txs.keys()) {
+			let indexes = split_txs.get(address);
 
-		let output_filename = path.format({
-			dir: path_comps.dir,
-			name: path_comps.name + '-' + options.from.toString() + '-' + options.to.toString(),
-			ext: path_comps.ext
-		});
+			let this_set_txs = indexes.map((tx_idx) => {
+				return txs[tx_idx];
+			});
 
-		await tools.storage.saveTransactionsToFile(output_filename, split_txs);
+			let output_filename = path.format({
+				dir: path_comps.dir,
+				name: path_comps.name + '-' + address,
+				ext: path_comps.ext
+			});
+
+			await tools.storage.saveTransactionsToFile(output_filename, this_set_txs);
+		}
 	}
 }
 
@@ -128,99 +139,69 @@ function parseCmdLineParams() {
 			return;
 		}
 
-		let count = null;
-		let quantity = null;
-		let _from = null;
-		let _to = null;
-		if (cmdline.keyexists('count')) {
-			if (cmdline.keyexists('quantity')) {
-				reject(new Error("ERROR: '--quantity' cannot be used with '--count' parameter."));
-				return;
-			}
-			if (cmdline.keyexists('from')) {
-				reject(new Error("ERROR: '--from' cannot be used with '--count' parameter."));
-				return;
-			}
-			if (cmdline.keyexists('to')) {
-				reject(new Error("ERROR: '--to' cannot be used with '--count' parameter."));
-				return;
-			}
+		let methods_count = 0;
+		if (cmdline.keyexists('by-count')) {
+			methods_count += 1;
+		}
+		if (cmdline.keyexists('by-quantity')) {
+			methods_count += 1;
+		}
+		if (cmdline.keyexists('by-sender')) {
+			methods_count += 1;
+		}
+		if (cmdline.keyexists('by-receiver')) {
+			methods_count += 1;
+		}
+		if (methods_count == 0) {
+			reject(new Error("ERROR: No split method specified."));
+			return;
+		}
+		if (methods_count != 1) {
+			reject(new Error("ERROR: More than one split method has been specified."));
+			return;
+		}
 
-			count = cmdline.get('count');
-			if (count === null) {
-				reject(new Error("ERROR: Missing value in '--count' parameter."));
+		let by_count = null;
+		let by_quantity = null;
+		let by_sender = null;
+		let by_receiver = null;
+		if (cmdline.keyexists('by-count')) {
+			by_count = cmdline.get('by-count');
+			if (by_count === null) {
+				reject(new Error("ERROR: Missing value in '--by-count' parameter."));
 				return;
 			}
-			count = parseInt(count, 10);
-			if (Number.isNaN(count) || count < 1) {
-				reject(new Error("ERROR: Invalid value in '--count' parameter. It must be greater or equal to 1."));
+			by_count = parseInt(by_count, 10);
+			if (Number.isNaN(by_count) || by_count < 1) {
+				reject(new Error("ERROR: Invalid value in '--by-count' parameter. It must be greater or equal to 1."));
 				return;
 			}
 		}
-		else if (cmdline.keyexists('quantity')) {
-			if (cmdline.keyexists('count')) {
-				reject(new Error("ERROR: '--count' cannot be used with '--quantity' parameter."));
+		else if (cmdline.keyexists('by-quantity')) {
+			by_quantity = cmdline.get('by-quantity');
+			if (by_quantity === null) {
+				reject(new Error("ERROR: Missing value in '--by-quantity' parameter."));
 				return;
 			}
-			if (cmdline.keyexists('from')) {
-				reject(new Error("ERROR: '--from' cannot be used with '--quantity' parameter."));
-				return;
-			}
-			if (cmdline.keyexists('to')) {
-				reject(new Error("ERROR: '--to' cannot be used with '--quantity' parameter."));
-				return;
-			}
-
-			quantity = cmdline.get('quantity');
-			if (quantity === null) {
-				reject(new Error("ERROR: Missing value in '--quantity' parameter."));
-				return;
-			}
-			quantity = parseInt(quantity, 10);
-			if (Number.isNaN(quantity) || quantity < 2) {
-				reject(new Error("ERROR: Invalid value in '--quantity' parameter. It must be greater or equal to 2."));
+			by_quantity = parseInt(by_quantity, 10);
+			if (Number.isNaN(by_quantity) || by_quantity < 2) {
+				reject(new Error("ERROR: Invalid value in '--by-quantity' parameter. It must be greater or equal to 2."));
 				return;
 			}
 		}
-		else {
-			if (cmdline.keyexists('quantity')) {
-				reject(new Error("ERROR: '--quantity' cannot be used with '--from' and '--to' parameters."));
-				return;
-			}
-			if (cmdline.keyexists('count')) {
-				reject(new Error("ERROR: '--count' cannot be used with '--from' and '--to' parameters."));
-				return;
-			}
-
-			_from = cmdline.get('from');
-			if (_from === null) {
-				reject(new Error("ERROR: Missing value in '--from' parameter."));
-				return;
-			}
-			_from = parseInt(_from, 10);
-			if (Number.isNaN(_from) || _from < 0) {
-				reject(new Error("ERROR: Invalid value in '--from' parameter. It must be greater or equal to 0."));
-				return;
-			}
-
-			_to = cmdline.get('to');
-			if (_to === null) {
-				reject(new Error("ERROR: Missing value in '--to' parameter."));
-				return;
-			}
-			_to = parseInt(_to, 10);
-			if (Number.isNaN(_to) || _to < _from) {
-				reject(new Error("ERROR: Invalid value in '--to' parameter. It must be greater or equal to '--from'."));
-				return;
-			}
+		else if (cmdline.keyexists('by-sender')) {
+			by_sender = true;
+		}
+		else if (cmdline.keyexists('by-receiver')) {
+			by_receiver = true;
 		}
 
 		resolve({
 			input,
-			count,
-			quantity,
-			from: _from,
-			to: _to,
+			by_count,
+			by_quantity,
+			by_sender,
+			by_receiver
 		});
 	});
 }
